f78aed1e979cd85b410692392a87fa72982b60e0
tungwuwt <tungwuwt@gmail.com>
2024-05-19 21:28:37 +0800

    NTree copy

--- SyntacticAnalysis/Visualization/NTree.h
+++ SyntacticAnalysis/Visualization/NTree.h
@@ -5,9 +5,21 @@
 #ifndef CODE_NTREE_H
 #define CODE_NTREE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 
 class NTree {
+    NTree(){};
+    NTree(std::string val) : val(val){}
 
+    static void printNTree(NTree* x, std::vector<bool> flag, int depth = 0, bool isLast = false);
+
+
+private:
+    std::string val;
+    std::vector<NTree*> child;
 };
 
 
