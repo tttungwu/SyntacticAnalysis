6d79f648161d2e9e6a264ab12b7256f6f6579ff5
tungwuwt <tungwuwt@gmail.com>
2024-05-20 20:18:31 +0800

    blank

--- main.cpp
+++ main.cpp
@@ -5,11 +5,8 @@
 
 int main()
 {
-//    Error::addError(ErrorType::LexicalError, "Unexpected character", 1, 5);
-//    Error::addError(ErrorType::SyntaxError, "Missing semicolon", 2, 10);
-//    Error::addError(ErrorType::SemanticError, "Undeclared variable", 3, 15);
-//    Error::addError(ErrorType::CodeGenError, "Invalid opcode", 4, 20);
-
     std::vector<Token> res = Lexer::tokenize("../test.txt");
     LL1Parser::parse(res);
+
+    return 0;
 }
